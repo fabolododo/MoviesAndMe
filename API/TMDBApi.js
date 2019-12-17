4211d622f4a3855e6520b4b2c8235278022914fe
FabriceL <fabrice.latri@hotmail.fr>
2019-12-17 20:13:57 +0100

    Scroll infini done and RAZ State avec callback done

--- API/TMDBApi.js
+++ API/TMDBApi.js
@@ -1,7 +1,7 @@
 const API_TOKEN = "0688017ac447dbc1d2a3f034474c1fc8";
 
-export function getFilmsFromApiWithSearchedText (text) {
-    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+export function getFilmsFromApiWithSearchedText (text, page) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text + "&page=" + page
     return fetch(url)
         .then((response) => response.json())
         .catch((error) => console.error(error))
