1ad3f6acac1ef55044c8e0b664e65754b9a153a0
FabriceL <fabrice.latri@hotmail.fr>
2019-12-21 18:44:55 +0100

    Added NavBar (V1.0)

--- navigation/Navigation.js
+++ navigation/Navigation.js
@@ -1,7 +1,11 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { StyleSheet, Image } from 'react-native';
 import Search from '../components/Search';
 import FilmDetail from '../components/FilmDetail';
+import Favorites from '../components/Favorites';
 
 const SearchStackNavigator = createStackNavigator({
     search: {
@@ -15,4 +19,60 @@ const SearchStackNavigator = createStackNavigator({
     }
 })
 
-export default createAppContainer(SearchStackNavigator);
+const MoviesTabNavigator = createBottomTabNavigator({
+    Search: {
+        screen: SearchStackNavigator,
+        navigationOptions: {
+            tabBarIcon: ({ tintColor }) => {
+                return <Image
+                    source={require('../assets/ic_search.png')}
+                    style={styles.icon}
+                    tintColor={{ tintColor }}
+                    />
+            }
+        }
+    },
+    Favorites: {
+        screen: Favorites,
+        navigationOptions: {
+            tabBarIcon: ({ tintColor }) => {
+                return <Image
+                    source={require('../assets/ic_favorite.png')}
+                    style={styles.icon}
+                    tintColor={{ tintColor }}
+                    />
+                }
+            }
+        }
+    },
+    {
+        tabBarOptions: {
+            activeBackgroundColor: '#744481',
+            activeTintColor: 'white',
+            inactiveTintColor: '#d8d8d8',
+            inactiveBackgroundColor: '#9973a2',
+            showLabel: true,
+            showIcon: true,
+            tabStyle: {
+                height: 100
+            },
+            labelStyle: {
+                paddingBottom: 30
+            },
+            style: {
+                marginTop: 10
+            }
+        }
+    }
+)
+
+const styles = StyleSheet.create({
+    icon: {
+      width: 30,
+      height: 30,
+      tintColor: 'white'
+
+    }
+  })
+
+export default createAppContainer(MoviesTabNavigator);
