81511e0991ad0c743dcdee247daec160bf10ba10
FabriceL <fabricel@MacBook-Pro-de-FabriceL.local>
2020-02-16 19:26:25 +0100

    ChangeThemeDark done

--- components/Theme.js
+++ components/Theme.js
@@ -6,22 +6,51 @@ import { Appearance, useColorScheme } from 'react-native-appearance';
 
 
 class Theme extends React.Component {
+
+    static navigationOptions = ({ navigation }) => {
+        const { params } = navigation.state
+
+        console.log('params', params);
+
+        if (params != undefined && params.toggleTheme === 'dark') { 
+            return {
+                headerStyle: {
+                    backgroundColor: '#333'
+                }
+            }
+        } else {
+            return {
+                headerStyle: {
+                    backgroundColor: '#fff'
+                }
+            }
+        }
+    }
+
     constructor(props) {
         super(props);
 
         console.log(Appearance.getColorScheme())
     }
 
+    componentDidMount() {
+        this.props.navigation.setParams({ toggleTheme: this.props.toggleTheme })
+    }
+
     _toggleChangeTheme() {
         let action
         if (this.props.toggleTheme == 'light') {
             action = { type: "TOGGLE_DARK" }
-            this.props.navigation.setParams({ toggleTheme: 'dark' });
         } else {
             action = { type: "TOGGLE_LIGHT" }
-            this.props.navigation.setParams({ toggleTheme: 'light' });
         }
         this.props.dispatch(action);
+        this._changeHeaderTheme();
+    }
+
+    _changeHeaderTheme () {
+        console.log('hh');
+        this.props.navigation.setParams({ toggleTheme: this.props.toggleTheme == 'dark' ? 'light' : 'dark' });
     }
 
     render () {
